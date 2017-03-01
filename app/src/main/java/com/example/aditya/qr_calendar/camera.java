e3aa6f02892b650f28c65d04e5daf43c055fa92d
Aditya <aditya@aditya.login.secureunibox.com>
2017-03-01 14:18:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/qr_calendar/camera.java
@@ -0,0 +1,25 @@
+package com.example.aditya.qr_calendar;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Hashtable;
+
+
+/**
+ * Created by aditya on 23/2/17.
+ */
+
+public class camera extends Activity {
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.camera);
+    }
+
+}
